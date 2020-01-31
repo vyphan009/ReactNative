9b668c975021110180dbc25db40e491a1e01c2ed
Vy Phan <vyphan.cnp@gmail.com>
2020-01-31 14:14:46 -0800

    pull data from youtube

--- App.js
+++ App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import { StackNavigator, NavigationActions } from 'react-navigation';
-import { StyleSheet, Text, View } from 'react-native';
-import HomeView from './containers/HomeView';
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Open up App.js to start working on your app!</Text>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-
-const App = StackNavigator({
-  HomeView: {screen: HomeView}
-
-});
+import React, { Component } from 'react';
+    import {
+      StyleSheet,
+      View,
+      StatusBar,
+    } from 'react-native';
+    import Routes from './Route';
+    export default class App extends Component<{}> {
+      render() {
+        return (
+          <View style={styles.container}>
+            <StatusBar
+              backgroundColor="#fff"
+              barStyle="dark-content"
+            />
+            <Routes/>
+          </View>
+        );
+      }
+    }
+    const styles = StyleSheet.create({
+      container : {
+        flex: 1,
+      }
+    }); 
